496e7419b1b69cc7edd4b2be051566ebde09bb30
razi-j <razaljosephandrew@gmail.com>
2025-09-22 15:58:23 +0800

    Utilized Switch Case

--- ToDoApp.java
+++ ToDoApp.java
@@ -1,67 +1,69 @@
-// ToDoApp.java
-import java.util.*;
+/// ToDoApp.java
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ToDoApp {
-  static ArrayList<String> tasks = new ArrayList<>(); // used generics for better stability
+    static ArrayList<String> tasks = new ArrayList<>(); // used generics for better stability
 
-  public static void addtask(String t) {
-    tasks.add(t);
-    System.out.println("Task Added!!");
-  }
-
-  public static void showTasks() {
-    if (tasks.size() == 0) {
-      System.out.println("No tasks yet");
-    } else {
-      for (int i = 0; i < tasks.size(); i++) {
-        System.out.println(
-            (i + 1) + ". " + tasks.get(i)); // fixed because i goes up to tasks.size()
-      } // say goodbye to java.lang.IndexOutOfBoundsException
+    public static void addtask(String t) {
+        tasks.add(t);
+        System.out.println("Task Added!!");
     }
+
+    public static void showTasks() {
+        if (tasks.isEmpty()) {
+        System.out.println("No tasks yet");
+        } 
+        else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(
+                (i + 1) + ". " + tasks.get(i)); // fixed because i goes up to tasks.size()
+            } // say goodbye to java.lang.IndexOutOfBoundsException
+        }
   }
 
-  public static void removeTask(int n) {
-    if (tasks.size()
-        == 0) { // fixed this function because it array is 0 indexed while display is 1 indexed
-      System.out.println("Nothing to remove");
-    } else if (n > 0
-        && n
-            <= tasks
-                .size()) { // previously if user wants to remove task 1, it will remove task 2
-                           // because of the array indexing
-      tasks.remove(n - 1);
-      System.out.println("Task removed");
-    } else {
-      System.out.println("Invalid task number");
-    }
+public static void removeTask(int n) {
+        if (tasks.isEmpty()) { // checks if list is empty then prints accordingly
+            System.out.println("Nothing to remove");
+        } 
+        else if (n > 0 && n <= tasks.size()) {
+            tasks.remove(n - 1);
+            System.out.println("Task removed");
+        } 
+        else {
+            System.out.println("Invalid task number");
+        }
   }
 
-  public static void main(String args[]) {
+public static void main(String args[]) {
     Scanner s = new Scanner(System.in);
     while (true) {
-      System.out.println("1 Add Task"); // <-- inconsistent spacing
-      System.out.println("2.Show Tasks");
-      System.out.println("3.Remove Task");
-      System.out.println("4 Exit");
-      int choice = s.nextInt();
-      s
-          .nextLine(); // added to consume leftover newline and to also allow adding tasks to
-                       // finally function again
-      if (choice == 1) {
-        System.out.println("Enter Task: ");
-        String t = s.nextLine();
-        addtask(t);
-      } else if (choice == 2) {
-        showTasks();
-      } else if (choice == 3) {
-        System.out.print("Enter task no to remove: ");
-        int n = s.nextInt();
-        removeTask(n);
-      } else if (choice == 4) {
-        break;
-      } else {
-        System.out.println("Wrong Choice!!");
-      }
+        System.out.println("1. Add Task"); // <-- inconsistent spacing
+        System.out.println("2. Show Tasks");
+        System.out.println("3. Remove Task");
+        System.out.println("4. Exit");
+        int choice = s.nextInt();
+        s.nextLine(); // added to consume leftover newline and to also allow adding tasks to
+                    // finally function again
+        switch(choice) {
+            case 1:
+                System.out.println("Enter Task: ");
+                String t = s.nextLine();
+                addtask(t);
+                
+            case 2:
+                showTasks();
+                
+            case 3:
+                System.out.print("Enter task no to remove: ");
+                int n = s.nextInt();
+                removeTask(n);
+                
+            case 4:
+                break;
+            default:
+                System.out.println("Wrong Choice!!");
+            }
+        }
     }
-  }
 }
