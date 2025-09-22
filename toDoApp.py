24a46be75bd8b44651a97ad96af6a266fc9b998d
Xavier Imbang <108415347+Chillynx@users.noreply.github.com>
2025-09-22 14:36:10 +0800

    Style & Naming
    Applied PEP8 formatting and renamed functions to snake_case

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,44 @@
-# toDoApp.py
+tasks = []
 
-tasks=[]
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
+def add_task(task):
+    tasks.append(task)
+    print("task added!")
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+
+def show_tasks():
+    if len(tasks) == 0:
+        print("no tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range(len(tasks)):
+            print(i + 1, ".", tasks[i])
 
-def removetask(tasknumber):
-    tasks.pop(tasknumber) 
+
+def remove_task(task_number):
+    tasks.pop(task_number)
     print("task removed!!")
+
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
         ch = input("enter choice : ")
-        if ch=="1":
+        if ch == "1":
             t = input("enter task : ")
-            addtask(t)
-        elif ch=="2":
-            showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+            add_task(t)
+        elif ch == "2":
+            show_tasks()
+        elif ch == "3":
+            n = int(input("enter task no to remove: "))
+            remove_task(n)
+        elif ch == "4":
+            break
         else:
             print("wrong choice!!")
-main()
+
+
+if __name__ == "__main__":
+    main()
